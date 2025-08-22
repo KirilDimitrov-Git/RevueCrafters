30f2b90942eb90faae04a3a412be9669401c4198
KirilDimitrov-Git <kirildimitrov1995@gmail.com>
2025-08-22 12:25:12 +0300

    Add files via upload

---
+++ RevueCraftersTests/Models/ApiResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RevueCraftersTests.Models
+{
+    class ApiResponseDTO
+    {
+        [JsonPropertyName("msg")]
+        public string? Msg { get; set; }
+
+        [JsonPropertyName("revueid")]
+        public string? RevueId { get; set; }
+    }
+}
---
+++ RevueCraftersTests/Models/RevueDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RevueCraftersTests.Models
+{
+    class RevueDTO
+    {
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+
+        [JsonPropertyName("url")]
+        public string? Url { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+    }
+}
